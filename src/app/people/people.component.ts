fed7d90ed6c193458ab66494bf46b1cba2879cda
Pavan Kumar Chaparla <pvnkumar137@.com>
2022-06-09 22:44:35 -0500

    added certain changes regarding the assignment tasks

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -13,13 +13,20 @@ export class PeopleComponent implements OnInit {
   click4=false;
   click5=false;
   click6=false;
+  click7 = false;
   searched=false;
   student:any;
   students = [
-    {"name":"dhruvi","salary":"99099","keywords":"dhruvi is nice", "picture":"https://webappsbm.blob.core.windows.net/webappsbm/dhru.jpg"},
+    {"name":"Dhruvi","salary":"99099","keywords":"dhruvi is nice", "picture":"https://webappsbm.blob.core.windows.net/webappsbm/dhru.jpg"},
     {"name":"Chuck", "salary":"10000","keywords":"Chuck is amazing", "picture":"https://webappsbm.blob.core.windows.net/webappsbm/chuck.jpg"},
     {"name":"Meena", "salary":"12500","keywords":"Meena is outta here", "picture":""},
-    {"name":"Dave", "salary":"20","keywords":"Who is this", "picture":"https://webappsbm.blob.core.windows.net/webappsbm/dave.jpg"}
+    {"name":"Dave", "salary":"20","keywords":"Who is this", "picture":"https://webappsbm.blob.core.windows.net/webappsbm/dave.jpg"},
+    {"name":"Tuan", "salary":"","keywords":"Tuan is gone", "picture":""},
+    {"name":"Tavo", "salary":"220200","keywords":"Tavo works very hard", "picture":"https://webappsbm.blob.core.windows.net/webappsbm/tavo.jpg"},
+    {"name":"Nora", "salary":"","keywords":"", "picture":""},
+    {"name":"Sue", "salary":"12500","keywords":"Sue isn't Susan", "picture":"https://webappsbm.blob.core.windows.net/webappsbm/upset.jpg"},
+    {"name":"Susan", "salary":"255000","keywords":"Susan is very smart", "picture":""},
+    {"name":"Darwin", "salary":"25","keywords":"Darwin is very creative", "picture":"https://webappsbm.blob.core.windows.net/webappsbm/dar.jpg"}
   ];
   salariedpeople:any;
   constructor() { }
@@ -51,6 +58,7 @@ export class PeopleComponent implements OnInit {
     this.click4 = false;
     this.click5 = false;
     this.click6 = false;
+    this.click7 = false;
   }
 
   salaried(){
@@ -60,6 +68,7 @@ export class PeopleComponent implements OnInit {
     this.click4 = false;
     this.click5 = false;
     this.click6 = false;
+    this.click7 = false;
     var temp = [];
     for(var i=0;i<this.students.length;i++){
       if(parseInt(this.students[i].salary)<99000){
@@ -76,6 +85,7 @@ export class PeopleComponent implements OnInit {
     this.click4 = false;
     this.click5 = false;
     this.click6 = false;
+    this.click7 = false;
 
   }
 
@@ -86,6 +96,7 @@ export class PeopleComponent implements OnInit {
     this.click1 = false;
     this.click5 = false;
     this.click6 = false;
+    this.click7 = false;
   }
 
   editKeywords(){
@@ -95,6 +106,7 @@ export class PeopleComponent implements OnInit {
     this.click3 = false;
     this.click1 = false;
     this.click6 = false;
+    this.click7 = false;
   }
 
   editSalaries(){
@@ -104,6 +116,34 @@ export class PeopleComponent implements OnInit {
     this.click3 = false;
     this.click1 = false;
     this.click5 = false;
+    this.click7 = false;
+  }
+
+  displayAll(){
+    this.click6 = false;
+    this.click4 = false;
+    this.click2 = false;
+    this.click3 = false;
+    this.click1 = false;
+    this.click5 = false;
+    this.click7 = true;
+  }
+
+  onFileChanged(event:any) {
+    const file = event.target.files[0];
+    const input = document.getElementById('nameForPicture') as HTMLInputElement | null;
+    for(var i=0;i<this.students.length;i++){
+      console.log(this.students[i].name,input?.value);
+      if(input?.value == this.students[i].name){
+        this.students[i].name = input.value;
+        this.students[i].picture = "../../assets/"+file.name;
+      }
+    }
+  }
+
+  changePicture(){
+    alert("Picture Changed Succesfully");
+
   }
 
   delete(){
